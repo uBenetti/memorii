9209e599974dc813e205c71592932211970e1ed7
uBenetti <pedrobenetti587@gmail.com>
2026-05-22 10:21:59 -0300

    autenticação com JWT implementada e rotas protegidas

--- back/notes/views.py
+++ back/notes/views.py
@@ -1,8 +1,18 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
-
 from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class RegisterView(generics.CreateAPIView):
     query=User.objects.all()
     serializer_class=RegisterSerializer
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        return Response({
+            'username': request.user.username
+        })
