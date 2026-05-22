6acc80b797fe8ae608503065128410bb2cb5b034
uBenetti <pedrobenetti587@gmail.com>
2026-05-22 09:46:05 -0300

    api de registro de usuário

--- back/notes/views.py
+++ back/notes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    query=User.objects.all()
+    serializer_class=RegisterSerializer
