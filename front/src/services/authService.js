8fa681f40d091904442d564beb11112c881b0168
uBenetti <pedrobenetti587@gmail.com>
2026-06-01 18:27:01 -0300

    authService.jsx verify automaticaly user infos

--- front/src/services/authService.js
+++ front/src/services/authService.js
@@ -1,5 +1,12 @@
 import api from "../api/api";
 
+export const login = async (username, password) => {
+  const response =await api.post("token/", {
+    username, password
+  });
+  return response.data
+}
+
 export const getProfile = async (token) => {
   const response = await api.get("profile/", {
     headers: {
