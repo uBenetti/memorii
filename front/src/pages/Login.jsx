9874cf3ade2a015fa8f7f5285a8d3a033451d028
uBenetti <pedrobenetti587@gmail.com>
2026-06-01 19:07:51 -0300

    Login create states, get input, load it to api and save the token

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,3 +1,48 @@
-export default function Register(){
-    return <h1>Login</h1>
+import { useState } from "react";
+import { login } from "../services/authService";
+
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try{
+            const data = await login(username, password);
+
+            localStorage.setItem("access", data.access);
+
+            alert("Login realizado com sucesso!");
+        } catch (error){
+            console.error(error);
+            alert("Usuário ou senha inválidos");
+        }
+    };
+
+    return(
+        <div>
+            <h1>Login</h1>
+
+            <input 
+                type="text"
+                placeholder="Usuário"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <br />
+            <br />
+            
+            <input 
+                type="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <br />
+            <br />
+
+            <button onClick={handleLogin}>Entrar</button>
+        </div>
+    );
 }
