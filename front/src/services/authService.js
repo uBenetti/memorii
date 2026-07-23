051ec1ef6eba98ccacb0927c028f30e83ddde436
Pedro Benetti <pedrobenetti587@gmail.com>
2026-07-23 12:50:16 +0000

    fix: configure Vite proxy for Codespaces API authentication

--- front/src/services/authService.js
+++ front/src/services/authService.js
@@ -1,11 +1,13 @@
 import api from "../api/api";
 
 export const login = async (username, password) => {
-  const response =await api.post("token/", {
-    username, password
+  const response = await api.post("token/", {
+    username,
+    password
   });
-  return response.data
-}
+
+  return response.data;
+};
 
 export const getProfile = async (token) => {
   const response = await api.get("profile/", {
